1e895a6b9d1b23c63dd20a76a83e1b19e2ad693b
Bruce Huang <bruce@Bruces-MacBook-Pro.local>
2022-10-13 22:35:30 -0700

    list_to_string | fix style check

--- list_to_string.py
+++ list_to_string.py
@@ -1,11 +1,13 @@
 from functools import reduce
 
+
 def list_to_string(input_list):
-	if isinstance(input_list, list) and len(input_list) > 0:
-		return reduce(lambda a, b: str(a)+"\n"+str(b), input_list)
-	else:
-		return ""
+    if isinstance(input_list, list) and len(input_list) > 0:
+        return reduce(lambda a, b: str(a) + "\n" + str(b), input_list)
+    else:
+        return ""
+
 
 if __name__ == '__main__':
-	print(list_to_string([]))
-	print(list_to_string(["khoury", "college", "align", "masters"]))
+    print(list_to_string([]))
+    print(list_to_string(["khoury", "college", "align", "masters"]))

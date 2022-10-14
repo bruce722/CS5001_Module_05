e632d1644eea74a5ad86fa4c15bac2a863904fde
Bruce Huang <bruce@Bruces-MacBook-Pro.local>
2022-10-13 22:40:40 -0700

    convert_tuple | fix style check

--- convert_tuple.py
+++ convert_tuple.py
@@ -1,6 +1,9 @@
+
+
 def convert_tuple(input_tuple):
-	return list(input_tuple)
+    return list(input_tuple)
+
 
 if __name__ == '__main__':
-	tuple1 = (1, 2, 4, 5, 6)
-	print(convert_tuple(tuple1))
+    tuple1 = (1, 2, 4, 5, 6)
+    print(convert_tuple(tuple1))

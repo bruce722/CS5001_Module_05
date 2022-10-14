329b25ecc508523e2367eb6d80ae5e4def10968a
Bruce Huang <bruce@Bruces-MacBook-Pro.local>
2022-10-13 22:29:50 -0700

    list_to_stirng | fix empty string case

--- list_to_string.py
+++ list_to_string.py
@@ -1,7 +1,10 @@
 from functools import reduce
 
-def list_to_string(list):
-	return reduce(lambda a, b: str(a)+"\n"+str(b), list)
+def list_to_string(input_list):
+	if isinstance(input_list, list):
+		return reduce(lambda a, b: str(a)+"\n"+str(b), input_list)
+	else:
+		return ""
 
 if __name__ == '__main__':
 	print(list_to_string(["khoury", "college", "align", "masters"]))

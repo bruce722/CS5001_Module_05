65afaebd8d0d967fb2e79d7dd1cd4577e82ec2b8
bruce722 <huangchuanzhao0722@gmail.com>
2022-10-13 23:58:20 -0700

    fibonacii | fix size 0 and 1

--- fibonacci.py
+++ fibonacci.py
@@ -2,8 +2,12 @@ from functools import reduce
 
 
 def fibonacci(n):
-    return reduce(lambda a, _: a+[a[-1]+a[-2]], range(n-2), [1, 1])
+    if n == 0:
+        return []
+    if n == 1:
+        return [1]
+    return reduce(lambda a, _: a + [a[-1] + a[-2]], range(n-2), [1, 1])
 
 
 if __name__ == '__main__':
-    print(fibonacci(10))
+    print(fibonacci(2))
